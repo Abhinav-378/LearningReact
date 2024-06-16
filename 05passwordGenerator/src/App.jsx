190d4684064a479304900421edb680197e83dec2
Abhinav-378 <abhichoukse05@gmail.com>
2024-06-16 20:43:08 +0530

    update clean code

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -1,46 +1,46 @@
-import { useState , useCallback,useEffect, useRef} from 'react'
+import { useState, useCallback, useEffect, useRef } from 'react'
 
 
 function App() {
-  const [length, setLength] = useState (8)
-  const[numberAllowed, setNumberAllowed] = useState(false)
-  const[charAllowed, setCharAllowed] = useState(false)
+  const [length, setLength] = useState(8)
+  const [numberAllowed, setNumberAllowed] = useState(false)
+  const [charAllowed, setCharAllowed] = useState(false)
   const [password, setPassword] = useState('')
 
-const passwordRef = useRef(null)
+  const passwordRef = useRef(null)
 
-  const pswdGenerator = useCallback(()=>{
+  const pswdGenerator = useCallback(() => {
     let pass = ""
     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 
-    if(numberAllowed){
-      str+="1234567890"
+    if (numberAllowed) {
+      str += "1234567890"
     }
 
-    if(charAllowed){
-      str+="!@#$%^&*{}[]`~"
+    if (charAllowed) {
+      str += "!@#$%^&*{}[]`~"
     }
     let char;
-    for(let i=1;i<=length;i++){
+    for (let i = 1; i <= length; i++) {
       char = Math.floor(Math.random() * str.length + 1)
       pass += str.charAt(char);
     }
 
     setPassword(pass)
 
-  }, 
+  },
     [length, numberAllowed, charAllowed, setPassword]
   )
 
-  const copyPassword = useCallback(()=>{
+  const copyPassword = useCallback(() => {
     passwordRef.current?.select()
     // passwordRef.current?.setSelectionRange(0,4)
 
     window.navigator.clipboard.writeText(password)
-  },[password])
-  useEffect(() =>{
+  }, [password])
+  useEffect(() => {
     pswdGenerator()
-  } ,[length, charAllowed, numberAllowed, pswdGenerator])
+  }, [length, charAllowed, numberAllowed, pswdGenerator])
 
   return (
     <>
@@ -55,10 +55,10 @@ const passwordRef = useRef(null)
             className='w-full py-1 px-3 '
             placeholder='password'
             readOnly
-            ref= {passwordRef}
+            ref={passwordRef}
           />
-          <button className='bg-blue-600 text-white px-3 py-0.5 shrink-0 ' 
-          onClick={copyPassword}
+          <button className='bg-blue-600 text-white px-3 py-0.5 shrink-0 '
+            onClick={copyPassword}
           >
             Copy
           </button>
@@ -66,34 +66,34 @@ const passwordRef = useRef(null)
         <div className='flex text-sm gap-x-2'>
           <div className='flex items-center gap-x-1'>
             <input type="range"
-            min={6}
-            max={24}
-            value = {length}
-            className='cursor-pointer ' 
-            onChange={(e) => {setLength(e.target.value)}}
+              min={6}
+              max={24}
+              value={length}
+              className='cursor-pointer '
+              onChange={(e) => { setLength(e.target.value) }}
             />
             <label>Length: {length}</label>
           </div>
 
           <div className="flex items-center gap-x-1">
-            <input type="checkbox" 
-            defaultChecked={numberAllowed}
-            id='numberInput'
-            onChange={()=>{
-              setNumberAllowed((prev)=>!prev)
-            }} />
+            <input type="checkbox"
+              defaultChecked={numberAllowed}
+              id='numberInput'
+              onChange={() => {
+                setNumberAllowed((prev) => !prev)
+              }} />
             <label htmlFor="numberInput">Numbers</label>
           </div>
 
 
           <div className="flex items-center gap-x-1">
             <input
-                type="checkbox"
-                defaultChecked={charAllowed}
-                id="characterInput"
-                onChange={() => {
-                    setCharAllowed((prev) => !prev )
-                }}
+              type="checkbox"
+              defaultChecked={charAllowed}
+              id="characterInput"
+              onChange={() => {
+                setCharAllowed((prev) => !prev)
+              }}
             />
             <label htmlFor="characterInput">Characters</label>
           </div>
